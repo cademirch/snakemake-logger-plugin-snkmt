59ae9dc9b1761b395fb63f3caca285d0dd6d66fc
Cade Mirchandani <cmirchan@ucsc.edu>
2025-09-20 13:26:59 -0700

    fix: changes for new snkmt version

--- src/snakemake_logger_plugin_snkmt/log_handler.py
+++ src/snakemake_logger_plugin_snkmt/log_handler.py
@@ -7,10 +7,11 @@ from datetime import datetime
 
 from snakemake_interface_logger_plugins.common import LogEvent
 from snakemake_interface_logger_plugins.settings import OutputSettingsLoggerInterface
-from snkmt.db.session import Database
-from snkmt.db.models.workflow import Workflow
-from snkmt.db.models.error import Error
-from snkmt.db.models.enums import Status
+from snkmt.core.db.session import Database
+from snkmt.core.db.version import DatabaseVersionError
+from snkmt.core.models.workflow import Workflow
+from snkmt.core.models.error import Error
+from snkmt.types.enums import Status
 
 
 from snakemake_logger_plugin_snkmt.event_handlers import (
@@ -47,7 +48,7 @@ class sqliteLogHandler(Handler):
         """
         super().__init__()
 
-        self.db_manager = Database(db_path=db_path, auto_migrate=False, create_db=True)
+        self.db_manager = Database(db_path=db_path, auto_migrate=True, create_db=True)
         self.common_settings = common_settings
 
         self.event_handlers: dict[str, EventHandler] = {  # type: ignore
@@ -117,43 +118,46 @@ class sqliteLogHandler(Handler):
 
     def close(self) -> None:
         """Close the handler and update the workflow status."""
-
-        if self.context.get("current_workflow_id"):
-            try:
-                with self.session_scope() as session:
-                    workflow = (
-                        session.query(Workflow)
-                        .filter(Workflow.id == self.context["current_workflow_id"])
-                        .first()
-                    )
-                    error = (
-                        session.query(Error)
-                        .filter(
-                            Error.workflow_id == self.context["current_workflow_id"]
+        # if db needs migrations, alembic messes with logging config and context is not set so this fails
+        try:
+            if hasattr(self, 'context') and self.context.get("current_workflow_id"):
+                try:
+                    with self.session_scope() as session:
+                        workflow = (
+                            session.query(Workflow)
+                            .filter(Workflow.id == self.context["current_workflow_id"])
+                            .first()
                         )
-                        .first()
+                        error = (
+                            session.query(Error)
+                            .filter(
+                                Error.workflow_id == self.context["current_workflow_id"]
+                            )
+                            .first()
+                        )
+
+                        if workflow:
+                            workflow.status = Status.UNKNOWN
+                            workflow.end_time = datetime.utcnow()
+
+                            if error:
+                                workflow.status = Status.ERROR
+                            elif workflow.progress >= 1:
+                                workflow.status = Status.SUCCESS
+
+                except Exception as e:
+                    self.handleError(
+                        logging.LogRecord(
+                            name="snkmtLogHandler",
+                            level=logging.ERROR,
+                            pathname="",
+                            lineno=0,
+                            msg=f"Error closing workflow: {str(e)}",
+                            args=(),
+                            exc_info=None,
+                        )
                     )
-
-                    if workflow:
-                        workflow.status = Status.UNKNOWN
-                        workflow.end_time = datetime.utcnow()
-
-                        if error:
-                            workflow.status = Status.ERROR
-                        elif workflow.progress >= 1:
-                            workflow.status = Status.SUCCESS
-
-            except Exception as e:
-                self.handleError(
-                    logging.LogRecord(
-                        name="snkmtLogHandler",
-                        level=logging.ERROR,
-                        pathname="",
-                        lineno=0,
-                        msg=f"Error closing workflow: {str(e)}",
-                        args=(),
-                        exc_info=None,
-                    )
-                )
+        except DatabaseVersionError:
+            pass
 
         super().close()
